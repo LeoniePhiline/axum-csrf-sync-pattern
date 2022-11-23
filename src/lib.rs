b8dad674a00109f92915fe116b3c3fa434df8883
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2022-11-23 20:03:16 +0100

    feat(docs): Add documentation, including doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,219 @@
+#![forbid(unsafe_code, future_incompatible)]
+#![deny(
+    missing_debug_implementations,
+    nonstandard_style,
+    missing_docs,
+    unreachable_pub,
+    missing_copy_implementations,
+    unused_qualifications
+)]
+//! # Axum Synchronizer Token Pattern CSRF prevention
+//!
+//! This crate provides a CSRF protection layer and middleware for use with the [axum](https://docs.rs/axum/) web framework.
+//!
+//! The middleware implements the [CSRF Synchronizer Token Pattern](https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#synchronizer-token-pattern)
+//! for AJAX backends and API endpoints as described in the OWASP CSRF prevention cheat sheet.
+//!
+//! ## Scope
+//!
+//! This middleware implements token transfer via [custom request headers](https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#use-of-custom-request-headers).
+//!
+//! The middleware requires and is built upon [`axum_sessions`](https://docs.rs/axum-sessions/), which in turn uses [`async_session`](https://docs.rs/async-session/).
+//!
+//! The [Same Origin Policy](https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy) prevents the custom request header to be set by foreign scripts.
+//!
+//! ### In which contexts should I use this middleware?
+//!
+//! The goal of this middleware is to prevent cross-site request forgery attacks specifically in applications communicating with their backend by means of the JavaScript
+//! [`fetch()` API](https://developer.mozilla.org/en-US/docs/Web/API/fetch) or classic [`XmlHttpRequest`](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest),
+//! traditionally called "AJAX".
+//!
+//! The Synchronizer Token Pattern is especially useful in [CORS](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS) contexts,
+//! as the underlying session cookie is obligatorily secured and inaccessible by JavaScript, while the custom HTTP response header carrying the CSRF token can be exposed
+//! using the CORS [`Access-Control-Expose-Headers`](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Expose-Headers) HTTP response header.
+//!
+//! While the [Same Origin Policy](https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy) commonly prevents custom request headers to be set on cross-origin requests,
+//! use of the use of the [Access-Control-Allow-Headers](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers) CORS HTTP response header
+//! can be used to specifically allow CORS requests to be equipped with a required custom HTTP request header.
+//!
+//! This approach ensures that requests forged by auto-submitted forms or other data-submitting scripts from foreign origins are unable to add the required header.
+//!
+//! ### When should I use other CSRF protection patterns or libraries?
+//!
+//! Use other available middleware libraries if you plan on submitting classical HTML forms without the use of JavaScript, and if you do not send the form data across origins.
+//!
+//! ## Security
+//! ### Token randomness
+//!
+//! The CSRF tokens are generated using [`rand::ThreadRng`](https://rust-random.github.io/rand/rand/rngs/struct.ThreadRng.html) which is considered cryptographically secure (CSPRNG).
+//! See ["Our RNGs"](https://rust-random.github.io/book/guide-rngs.html#cryptographically-secure-pseudo-random-number-generators-csprngs) for more.
+//!
+//! ### Underlying session security
+//!
+//! The security of the underlying session is paramount - the CSRF prevention methods applied can only be as secure as the session carrying the server-side token.
+//!
+//! - When creating your [SessionLayer](https://docs.rs/axum-sessions/latest/axum_sessions/struct.SessionLayer.html), make sure to use at least 64 bytes of cryptographically secure randomness.
+//! - Do not lower the secure defaults: Keep the session cookie's `secure` flag **on**.
+//! - Use the strictest possible same-site policy.
+//!
+//! ### CORS security
+//!
+//! If you need to provide and secure cross-site requests:
+//!
+//! - Allow only your backend origin when configuring the [`CorsLayer`](https://docs.rs/tower-http/latest/tower_http/cors/struct.CorsLayer.html)
+//! - Allow only the headers you need. (At least the CSRF request token header.)
+//! - Only expose the headers you need. (At least the CSRF response token header.)
+//!
+//! ### No leaks of error details
+//!
+//! Errors are logged using [`tracing::error!`]. Error responses do not contain error details.
+//!
+//! Use [`tower_http::TraceLayer`](https://docs.rs/tower-http/latest/tower_http/trace/struct.TraceLayer.html) to capture and view traces.
+//!
+//! ## Safety
+//!
+//! This crate uses no `unsafe` code.
+//!
+//! The layer and middleware functionality is tested. View the the module source code to learn more.
+//!
+//! ## Usage
+//!
+//! ### Same-site usage
+//!
+//! Configure your session and CSRF protection layer in your backend application:
+//!
+//! ```
+//! use rand::RngCore;
+//!
+//! let mut secret = [0; 64];
+//! rand::thread_rng().try_fill_bytes(&mut secret).unwrap();
+//!
+//! async fn handler() -> http::StatusCode {
+//!     http::StatusCode::OK
+//! }
+//!
+//! let app = axum::Router::new()
+//!     .route("/", axum::routing::get(handler).post(handler))
+//!     .layer(
+//!         axum_csrf_sync_pattern::CsrfSynchronizerTokenLayer::default()
+//!
+//!         // Optionally, configure the layer with the following options:
+//!
+//!         // Default: RegenerateToken::PerSession
+//!         .regenerate(axum_csrf_sync_pattern::RegenerateToken::PerUse)
+//!         // Default: "X-CSRF-TOKEN"
+//!         .request_header("X-Custom-CSRF-Token-Client-Request-Header")
+//!         // Default: "X-CSRF-TOKEN"
+//!         .response_header("X-Custom-CSRF-Token-Server-Response-Header")
+//!         // Default: "_csrf_token"
+//!         .session_key("_custom_csrf_token_session_key")
+//!     )
+//!     .layer(
+//!         axum_sessions::SessionLayer::new(
+//!             async_session::MemoryStore::new(),
+//!             &secret
+//!         )
+//!     );
+//!
+//! // Use hyper to run `app` as service and expose on a local port or socket.
+//!
+//! # use tower::util::ServiceExt;
+//! # tokio_test::block_on(async {
+//! #     app.oneshot(
+//! #         http::Request::builder().body(axum::body::Body::empty()).unwrap()
+//! #     ).await.unwrap();
+//! # })
+//! ```
+//!
+//! Receive the token and send same-site requests, using your custom header:
+//!
+//! ```javascript
+//! // Receive CSRF token (Default response header name: 'X-CSRF-TOKEN')
+//! const token = (await fetch('/')).headers.get('X-Custom-CSRF-Token-Server-Response-Header');
+//!
+//! // Submit data using the token
+//! await fetch('/' {
+//!     method: 'POST',
+//!     headers: {
+//!        'Content-Type': 'application/json',
+//!        // Default request header name: 'X-CSRF-TOKEN'
+//!        'X-Custom-CSRF-Token-Client-Request-Header': token,
+//!     },
+//!     body: JSON.stringify({ /* ... */ }),
+//! });
+//! ```
+//!
+//! ### CORS-enabled usage
+//!
+//! Configure your CORS layer, session and CSRF protection layer in your backend application:
+//!
+//! ```
+//! use rand::RngCore;
+//!
+//! let mut secret = [0; 64];
+//! rand::thread_rng().try_fill_bytes(&mut secret).unwrap();
+//!
+//! async fn handler() -> http::StatusCode {
+//!     http::StatusCode::OK
+//! }
+//!
+//! let app = axum::Router::new()
+//!     .route("/", axum::routing::get(handler).post(handler))
+//!     .layer(
+//!         // See example above for custom layer configuration.
+//!         axum_csrf_sync_pattern::CsrfSynchronizerTokenLayer::default()
+//!     )
+//!     .layer(
+//!         axum_sessions::SessionLayer::new(
+//!             async_session::MemoryStore::new(),
+//!             &secret
+//!         )
+//!     )
+//!     .layer(
+//!         tower_http::cors::CorsLayer::new()
+//!             .allow_origin(tower_http::cors::AllowOrigin::list(["https://www.example.com".parse().unwrap()]))
+//!             .allow_methods([http::Method::GET, http::Method::POST])
+//!             .allow_headers([http::header::CONTENT_TYPE, "X-CSRF-TOKEN".parse().unwrap()])
+//!             .allow_credentials(true)
+//!             .expose_headers(["X-CSRF-TOKEN".parse().unwrap()]),
+//!    );
+//!
+//! // Use hyper to run `app` as service and expose on a local port or socket.
+//!
+//! # use tower::util::ServiceExt;
+//! # tokio_test::block_on(async {
+//! #     app.oneshot(
+//! #         http::Request::builder().body(axum::body::Body::empty()).unwrap()
+//! #     ).await.unwrap();
+//! # })
+//! ```
+//!
+//! Receive the token and send cross-site requests, using your custom header:
+//!
+//! ```javascript
+//! // Receive CSRF token
+//! const token = (await fetch('https://backend.example.com/', {
+//!     credentials: 'include',
+//! })).headers.get('X-CSRF-TOKEN');
+//!
+//! // Submit data using the token
+//! await fetch('https://backend.example.com/' {
+//!     method: 'POST',
+//!     headers: {
+//!        'Content-Type': 'application/json',
+//!        'X-CSRF-TOKEN': token,
+//!     },
+//!     credentials: 'include',
+//!     body: JSON.stringify({ /* ... */ }),
+//! });
+//! ```
+//!
+//!
+//! ## Contributing
+//!
+//! Pull requests are welcome!
+//!
+
 use std::{
     convert::Infallible,
     future::Future,
@@ -13,11 +229,24 @@ use rand::RngCore;
 use tokio::sync::RwLockWriteGuard;
 use tower::Layer;
 
-#[derive(Clone, Debug)]
+/// Use `CsrfSynchronizerTokenLayer::default()` to provide the middleware and configuration to axum's service stack.
+///
+/// Use the provided methods to configure details, such as when tokens are regenerated, what request and response
+/// headers should be named, and under which key the token should be stored in the session.
+#[derive(Clone, Copy, Debug)]
 pub struct CsrfSynchronizerTokenLayer {
+    /// Configures when tokens are regenerated: Per session, per use or per request. See [`RegenerateToken`] for details.
     pub regenerate_token: RegenerateToken,
+
+    /// Configures the request header name accepted by the middleware. Defaults to `"X-CSRF-TOKEN"`.
+    /// This header is set on your JavaScript or WASM requests originating from the browser.
     pub request_header: &'static str,
+
+    /// Configures the response header name sent by the middleware. Defaults to `"X-CSRF-TOKEN"`.
+    /// This header is received by your JavaScript or WASM code and its name must be used to extract the token from the HTTP response.
     pub response_header: &'static str,
+
+    /// Configures the key under which the middleware stores the server-side token in the session. Defaults to `"_csrf_token"`.
     pub session_key: &'static str,
 }
 
@@ -27,27 +256,38 @@ impl Default for CsrfSynchronizerTokenLayer {
             regenerate_token: Default::default(),
             request_header: "X-CSRF-TOKEN",
             response_header: "X-CSRF-TOKEN",
-            session_key: "csrf_token",
+            session_key: "_csrf_token",
         }
     }
 }
 
 impl CsrfSynchronizerTokenLayer {
+    /// Configure when tokens are regenerated: Per session, per use or per request. See [`RegenerateToken`] for details.
     pub fn regenerate(mut self, regenerate_token: RegenerateToken) -> Self {
         self.regenerate_token = regenerate_token;
 
         self
     }
+
+    /// Configure a custom request header name accepted by the middleware. Defaults to `"X-CSRF-TOKEN"`.
+    ///
+    /// This header is set on your JavaScript or WASM requests originating from the browser.
     pub fn request_header(mut self, request_header: &'static str) -> Self {
         self.request_header = request_header;
 
         self
     }
+
+    /// Configure a custom response header name sent by the middleware. Defaults to `"X-CSRF-TOKEN"`.
+    ///
+    /// This header is received by your JavaScript or WASM code and its name must be used to extract the token from the HTTP response.
     pub fn response_header(mut self, response_header: &'static str) -> Self {
         self.response_header = response_header;
 
         self
     }
+
+    /// Configure a custom key under which the middleware stores the server-side token in the session. Defaults to `"_csrf_token"`.
     pub fn session_key(mut self, session_key: &'static str) -> Self {
         self.session_key = session_key;
 
@@ -82,16 +322,37 @@ impl<S> Layer<S> for CsrfSynchronizerTokenLayer {
     type Service = CsrfSynchronizerTokenMiddleware<S>;
 
     fn layer(&self, inner: S) -> Self::Service {
-        CsrfSynchronizerTokenMiddleware::new(inner, self.clone())
+        CsrfSynchronizerTokenMiddleware::new(inner, *self)
     }
 }
 
+/// This enum is used with [`CsrfSynchronizerTokenLayer::regenerate`] to determine
+/// at which occurences the CSRF token should be regenerated.
+///
+/// You could understand these options as modes to choose a level of paranoia, depending on your application's requirements.
+///
+/// This paranoia level is a trade-off between ergonomics and security; as more frequent
+/// token invalidation requires more overhead for handling and renewing tokens on the client side,
+/// as well as retrying requests with a fresh token, should they fail.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
 #[allow(clippy::enum_variant_names)]
 pub enum RegenerateToken {
+    /// Generate one CSRF token per session and use this token until the session ends.
+    ///
+    /// This is the default behavior and should work for most applications.
     #[default]
     PerSession,
+    /// Regenerate the CSRF token after each use. A "use" describes an unsafe HTTP method
+    /// (`POST`, `PUT`, `PATCH`, `DELETE`).
+    ///
+    /// CSRF tokens are not required for, and thus not invalidated by handling requests
+    /// using safe HTTP methods (`HEAD`, `GET`, `OPTIONS`, `TRACE`, `CONNECT`).
     PerUse,
+    /// Regenerate the CSRF token at each request, including safe HTTP methods (`HEAD`, `GET`, `OPTIONS`, `TRACE`, `CONNECT`).
+    ///
+    /// This behavior might require elaborate token handling on the client side,
+    /// as any concurrent requests mean race conditions from the client's perspective,
+    /// and each request's response yields a new token to be used on the consecutive request.
     PerRequest,
 }
 
@@ -120,11 +381,17 @@ impl IntoResponse for Error {
     }
 }
 
-/// Verify the CSRF token header on the incoming request.
-/// This middleware receives a CSRF token as `X-CSRF-TOKEN` HTTP request header value
+/// This middleware is created by axum by applying the `CsrfSynchronizerTokenLayer`.
+/// It verifies the CSRF token header on incoming requests, regenerates tokens as configured,
+/// and attaches the current token to the outgoing response.
+///
+/// In detail, this middleware receives a CSRF token as `X-CSRF-TOKEN` (if not custom configured
+/// with a different name) HTTP request header value
 /// and compares it to the token stored in the session.
+///
 /// Upon response from the inner service, the session token is returned to the
 /// client via the `X-CSRF-TOKEN` response header.
+///
 /// Make sure to expose this header in your CORS configuration if necessary!
 ///
 /// Requires and uses `axum_sessions`.
@@ -143,6 +410,8 @@ pub struct CsrfSynchronizerTokenMiddleware<S> {
 }
 
 impl<S> CsrfSynchronizerTokenMiddleware<S> {
+    /// Create a new middleware from an inner [`tower::Service`] (axum-specific bounds, such as `Infallible` errors apply!) and a [`CsrfSynchronizerTokenLayer`].
+    /// Commonly, the middleware is created by the [`tower::Layer`] - and never manually.
     pub fn new(inner: S, layer: CsrfSynchronizerTokenLayer) -> Self {
         CsrfSynchronizerTokenMiddleware { inner, layer }
     }
@@ -165,7 +434,7 @@ where
     fn call(&mut self, req: Request<B>) -> Self::Future {
         let clone = self.inner.clone();
         let mut inner = std::mem::replace(&mut self.inner, clone);
-        let layer = self.layer.clone();
+        let layer = self.layer;
         Box::pin(async move {
             let session_handle = match req
                 .extensions()
